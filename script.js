b0acbd3c2c295ac25d8fceb996b7fe8f04ae61f0
Kieran Cattermole <kctffs@gmail.com>
2024-04-10 01:51:13 +0000

    made questions visable from js commands

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let startQuizBtn = document.getElementById('start-btn');
-let displayQuestion = document.getElementById('quiz-questions')
+let displayQuestion = document.getElementById('quiz-questions');
+let questionData = document.getElementById('question');
+let answerData = document.getElementById('answer-btns');
+let presentQuestion;
 
 startQuizBtn.addEventListener('click', startQuiz);
 
 function startQuiz() {
     console.log('Started Country Quiz');
     startQuizBtn.style.display = "none";
+    presentQuestion = questions.sort();
+    presentQuestionI = 0;
     displayQuestion.classList.remove('vanish');
-
+    quizQuestion();
 }
 
 function quizQuestion() {
+    seeableQuestion(presentQuestion[presentQuestionI]);
+}
+
+function seeableQuestion(question) {
+    questionData.innerText = question.question;
 
 }
 
@@ -21,7 +31,7 @@ function chosenAnswer() {
 /* Quiz questions */
 let questions = [
     {
-        question: 'What country is the most populated? (from 2024)',
+        question: '1. What country is the most populated? (from 2024)',
         answers: [
             { text: 'China', correct: false },
             { text: 'India', correct: true },
@@ -30,7 +40,7 @@ let questions = [
         ]
     },
     {
-        question: 'What animal does Bhutan have on their flag?',
+        question: '2. What animal does Bhutan have on their flag?',
         answers: [
             { text: 'Dragon', correct: true },
             { text: 'Horse', correct: false },
@@ -39,7 +49,7 @@ let questions = [
         ]
     },
     {
-        question: 'What is the biggest country by area?',
+        question: '3. What is the biggest country by area?',
         answers: [
             { text: 'Canada', correct: false },
             { text: 'Russia', correct: true },
@@ -48,7 +58,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country has never fought in a war?',
+        question: '4. What country has never fought in a war?',
         answers: [
             { text: 'Iceland', correct: false },
             { text: 'Sweden', correct: false },
@@ -57,7 +67,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country has the most colours on their flag?',
+        question: '5. What country has the most colours on their flag?',
         answers: [
             { text: 'South Africa', correct: false },
             { text: 'Central African Republic', correct: false },
@@ -66,7 +76,7 @@ let questions = [
         ]
     },
     {
-        question: "What is the world's newest country? (recognised independence from inital country)",
+        question: "6. What is the world's newest country? (recognised independence from inital country)",
         answers: [
             { text: 'Montenegro', correct: false },
             { text: 'Serbia', correct: false },
@@ -75,7 +85,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country has the highest female to male ratio? (from 2024)',
+        question: '7. What country has the highest female to male ratio? (from 2024)',
         answers: [
             { text: 'Armenia', correct: true },
             { text: 'Ukraine', correct: false },
@@ -84,7 +94,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country has the most islands being over 250,000?',
+        question: '8. What country has the most islands being over 250,000?',
         answers: [
             { text: 'Sweden', correct: true },
             { text: 'Philippines', correct: false },
@@ -93,7 +103,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country has the longest coastline?',
+        question: '9. What country has the longest coastline?',
         answers: [
             { text: 'Croatia', correct: false },
             { text: 'Canada', correct: true },
@@ -102,7 +112,7 @@ let questions = [
         ]
     },
     {
-        question: 'What country is the most visited in the world? (from 2024)',
+        question: '10. What country is the most visited in the world? (from 2024)',
         answers: [
             { text: 'Mexico', correct: false },
             { text: 'Turkey', correct: false },
